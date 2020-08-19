8cf17caaf0763bc4c5578c7c2d164b515d2f28f6
rosimar vieira campos <rvcdeveloper@gmail.com>
2020-08-19 19:04:39 -0300

    update

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.resolve(__dirname)))
 app.use(express.json())
 
